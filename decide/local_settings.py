bc30af684759ea290b8bcb9a5c0598a470e48280
AlfonsoRodriguezRomero <43932793+AlfonsoRodriguezRomero@users.noreply.github.com>
2019-11-28 20:25:17 +0100

    Actualizado local_settings.py
    Arreglados errores.

--- decide/local_settings.py
+++ decide/local_settings.py
@@ -14,7 +14,6 @@ MODULES = [
 ]
 
 APIS = {
-<<<<<<< HEAD
     'authentication':'http://localhost:8000',
     'base':'http://localhost:8000',
     'booth':'http://localhost:8000',
@@ -24,31 +23,11 @@ APIS = {
     'store':'http://localhost:8000',
     'visualizer':'http://localhost:8000',
     'voting':'http://localhost:8000',
-=======
-    'authentication': 'http://localhost:8000',
-    'base': 'http://localhost:8000',
-    'booth': 'http://localhost:8000',
-    'census': 'http://localhost:8000',
-    'mixnet': 'http://localhost:8000',
-    'postproc': 'http://localhost:8000',
-    'store': 'http://localhost:8000',
-    'visualizer': 'http://localhost:8000',
-    'voting': 'http://localhost:8000',
->>>>>>> ee6725a5ba89fb230131d6159311989fec2f346b
 }
 
 BASEURL = 'http://localhost:8000'
 
 DATABASES = {
-<<<<<<< HEAD
-    'default' : {
-        'ENGINE':'django.db.backends.postgresql',
-        'NAME':'decide',
-        'USER':'decide',
-        'PASSWORD':'decide',
-        'HOST':'localhost',
-        'PORT':'5432',
-=======
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'decide',
@@ -56,7 +35,6 @@ DATABASES = {
         'PASSWORD': 'decide',
         'HOST': 'localhost',
         'PORT': '5432',
->>>>>>> ee6725a5ba89fb230131d6159311989fec2f346b
     }
 }
 
