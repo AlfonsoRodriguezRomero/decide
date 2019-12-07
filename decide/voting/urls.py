0c26ab1b27417c771f3913371d63bedd2b384afd
danielhingar <danielhingar3397@gmail.com>
2019-12-07 21:10:29 +0100

    Mejora interfaz más amigable
    Se ha añadido una vista nueva para crear y editar votaciones, con motivo
    de tener una interfaz más vistosa. Para ello se ha creado un formulario y se
    ha añadido dos nuevos métodos en views.py. Además se ha creado las plantillas
    voting_add.html y edit_voting.html.

--- decide/voting/urls.py
+++ decide/voting/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from . import views
-
+from . import views 
+from .views import voting_list,add_voting, edit_voting
 
 
 urlpatterns = [
    # path('', views.VotingView.as_view(), name='voting'),
-   # path('<int:voting_id>/', views.VotingUpdate.as_view(), name='voting'),
-    path('', views.VotingView.voting_list,name='list_voting'),
+    path('<int:voting_id>/', views.VotingUpdate.as_view(), name='voting'),
+    path('', voting_list,name='list_voting'),
+    path('add_voting/',add_voting,name="add_voting"),
+    path('edit_voting/<int:id>',edit_voting)
 ]
  
--- decide/voting/views.py
+++ decide/voting/views.py
@@ -9,9 +9,12 @@ from .models import Question, QuestionOption, Voting
 from .serializers import SimpleVotingSerializer, VotingSerializer
 from base.perms import UserIsStaff
 from base.models import Auth
-from django.shortcuts import render
-import json
+from django.shortcuts import render,redirect
 from django.db.models import Q
+from .form import VotingForm
+from django.contrib import messages
+
+
 
 class VotingView(generics.ListCreateAPIView):
     queryset = Voting.objects.all()
@@ -19,16 +22,7 @@ class VotingView(generics.ListCreateAPIView):
     filter_backends = (django_filters.rest_framework.DjangoFilterBackend,)
     filter_fields = ('id', )
 
-    def voting_list(request):
-        votings = Voting.objects.all()
-        queryset = request.GET.get('campo')
-        if queryset:
-            votings= Voting.objects.filter(
-                Q(name__icontains=queryset)|
-                Q(id__icontains=queryset)
-            )
-        return render(request,'voting/voting.html',{'votings':votings})
-
+    
     def get(self, request, *args, **kwargs):
         version = request.version
         if version not in settings.ALLOWED_VERSIONS:
@@ -114,4 +108,32 @@ class VotingUpdate(generics.RetrieveUpdateDestroyAPIView):
             st = status.HTTP_400_BAD_REQUEST
         return Response(msg, status=st)
 
+def voting_list(request):
+        votings = Voting.objects.all()
+        queryset = request.GET.get('campo')
+        if queryset:
+            votings= Voting.objects.filter(
+                Q(name__icontains=queryset)|
+                Q(id__icontains=queryset)
+            )
+        return render(request,'voting/voting.html',{'votings':votings})
 
+
+def add_voting(request):
+    form= VotingForm(request.POST)
+    if form.is_valid():
+        vo = form.save()
+        vo.save()
+        form=VotingForm
+       
+        return redirect('list_voting')
+    return render(request,"voting/voting_add.html",{'form':form})
+
+def edit_voting(request,id=None):
+    voting=get_object_or_404(Voting,id=id)
+    form = VotingForm(request.POST or None,instance=voting)
+    if form.is_valid():
+        vo= form.save()
+        vo.save()
+        return redirect('list_voting')
+    return render(request,"voting/voting_edit.html",{'form':form})
