f5124d85df8173267c90e81a14cd4fce71bed408
LorenRD <lrondan95@gmail.com>
2019-12-13 00:05:17 +0100

    Fecha Inicio/Fin Votacion: Revision
    -decide/voting/form.py: añadidos start_date y end_date a los widgets
    -voting_add.html: añadidos start_date y end_date y scripts para pulsadores

--- decide/voting/form.py
+++ decide/voting/form.py
@@ -7,6 +7,6 @@ class VotingForm(forms.ModelForm):
         fields = ['name','desc','question','auths', 'start_date', 'end_date']
         widgets = {
             'name': forms.TextInput(attrs={'placeholder':'Voting name'}),
-            'start_date': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
-            'end_date': forms.DateTimeInput(attrs={'type': 'datetime-local'})
+            'start_date': forms.DateInput(attrs={'id': 'datetime-local'}),
+            'end_date': forms.DateInput(attrs={'id': 'datetime-local-2'})
         }
