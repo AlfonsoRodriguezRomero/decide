0dbb43f9ae5c2d3303f37588770c56249f195012
Loren <lrondan95@gmail.com>
2019-12-12 11:55:06 +0100

    Fecha inicio y fin: Arreglos
    form.py
    voting_add.html

--- decide/voting/form.py
+++ decide/voting/form.py
@@ -4,9 +4,9 @@ from .models import Voting
 class VotingForm(forms.ModelForm):
     class Meta:
         model = Voting
-        fields = ['name','desc','question','auths']
+        fields = ['name','desc','question','auths', 'start_date', 'end_date']
         widgets = {
             'name': forms.TextInput(attrs={'placeholder':'Voting name'}),
-            
-            
+            'start_date': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+            'end_date': forms.DateTimeInput(attrs={'type': 'datetime-local'})
         }
