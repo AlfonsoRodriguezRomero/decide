6f7be1cebd93bedb8e518f3904bd568c1b8da0fa
AlfonsoRodriguezRomero <43932793+AlfonsoRodriguezRomero@users.noreply.github.com>
2019-12-28 12:48:28 +0100

    Update urls.py

--- decide/decide/urls.py
+++ decide/decide/urls.py
@@ -22,7 +22,6 @@ from rest_framework_swagger.views import get_swagger_view
 schema_view = get_swagger_view(title='Decide API')
 
 urlpatterns = [
-    path('', views.index, name="index"),
     path('admin/', admin.site.urls),
     path('doc/', schema_view),
     path('gateway/', include('gateway.urls')),
