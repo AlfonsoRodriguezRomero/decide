0ae6c3f645137acfaae6dd9a1bc360ea396870f6
danielhingar <danielhingar3397@gmail.com>
2020-01-11 20:21:38 +0100

    Test selenium-Finalizado
    Se ha modificado el archivo test_selenium.py para que utilize las urls de la página desplegada en heroku

--- decide/voting/test_selenium.py
+++ decide/voting/test_selenium.py
@@ -7,11 +7,11 @@ class TestListAndSearch(unittest.TestCase):
         self.driver = webdriver.Firefox()
         
     def test(self):
-        self.driver.get("http://localhost:8000/admin/")
+        self.driver.get("http://decide-zapdos-votacion.herokuapp.com/admin/")
         self.driver.find_element_by_id("id_username").send_keys("decide")
-        self.driver.find_element_by_id("id_password").send_keys("decide1234")
+        self.driver.find_element_by_id("id_password").send_keys("passdecide")
         self.driver.find_element_by_css_selector("input[type='submit']").click()
-        self.driver.get("http://localhost:8000/voting/")
+        self.driver.get("http://decide-zapdos-votacion.herokuapp.com/voting/")
         self.driver.find_element_by_name("campo").click()
         self.driver.find_element_by_name("campo").send_keys("p")
         self.driver.find_element_by_css_selector(".btn-outline-success").click()
@@ -27,11 +27,11 @@ class TestCreate(unittest.TestCase):
         self.driver = webdriver.Firefox()
   
     def test_testCreate(self):
-        self.driver.get("http://localhost:8000/admin/")
+        self.driver.get("http://decide-zapdos-votacion.herokuapp.com/admin/")
         self.driver.find_element_by_id("id_username").send_keys("decide")
-        self.driver.find_element_by_id("id_password").send_keys("decide1234")
+        self.driver.find_element_by_id("id_password").send_keys("passdecide")
         self.driver.find_element_by_css_selector("input[type='submit']").click()
-        self.driver.get("http://localhost:8000/voting/")
+        self.driver.get("http://decide-zapdos-votacion.herokuapp.com/voting/")
         self.driver.find_element_by_link_text("Create voting").click()
         self.driver.find_element_by_id("id_name").click()
         self.driver.find_element_by_id("id_name").send_keys("test1")
@@ -54,11 +54,11 @@ class TestDelete(unittest.TestCase):
         self.driver = webdriver.Firefox()
   
     def test_delete(self):
-        self.driver.get("http://localhost:8000/admin/")
+        self.driver.get("http://decide-zapdos-votacion.herokuapp.com/admin/")
         self.driver.find_element_by_id("id_username").send_keys("decide")
-        self.driver.find_element_by_id("id_password").send_keys("decide1234")
+        self.driver.find_element_by_id("id_password").send_keys("passdecide")
         self.driver.find_element_by_css_selector("input[type='submit']").click()
-        self.driver.get("http://localhost:8000/voting/")
+        self.driver.get("http://decide-zapdos-votacion.herokuapp.com/voting/")
         self.driver.find_element_by_css_selector("tr:nth-child(2) .btn-danger").click()
         self.driver.find_element_by_css_selector(".btn-danger").click()
         self.assertTrue(self.driver.find_elements_by_class_name("card-header"))
@@ -73,12 +73,12 @@ class TestEdit(unittest.TestCase):
         self.driver = webdriver.Firefox()
   
     def test_edit(self):
-        self.driver.get("http://localhost:8000/admin/")
+        self.driver.get("http://decide-zapdos-votacion.herokuapp.com/admin/")
         self.driver.find_element_by_id("id_username").send_keys("decide")
-        self.driver.find_element_by_id("id_password").send_keys("decide1234")
+        self.driver.find_element_by_id("id_password").send_keys("passdecide")
         self.driver.find_element_by_css_selector("input[type='submit']").click()
-        self.driver.get("http://localhost:8000/voting/")
-        self.driver.find_element_by_css_selector("tr:nth-child(2) .btn-warning").click()
+        self.driver.get("http://decide-zapdos-votacion.herokuapp.com/voting/")
+        self.driver.find_element_by_css_selector("tr:nth-child(1) .btn-warning").click()
         self.driver.find_element_by_id("id_name").click()
         self.driver.find_element_by_id("id_name").send_keys("pp")
         self.driver.find_element_by_css_selector(".btn").click()
