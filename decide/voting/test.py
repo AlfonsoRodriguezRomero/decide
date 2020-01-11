47465f2915cff93db6ab623842895e648b838734
Loren <lrondan95@gmail.com>
2020-01-11 13:35:25 +0100

    Prueba Travis Selenium

--- decide/voting/test.py
+++ decide/voting/test.py
@@ -9,11 +9,11 @@ class TestDeleteQuestion():
         self.driver.quit
 
     def test_deleteQuestion(self):
-        self.driver.get("http://127.0.0.1:8000/admin/")
+        self.driver.get("http://decide-zapdos-votacion.herokuapp.com/admin/")
         self.driver.find_element_by_id('id_username').send_keys("decide")
         self.driver.find_element_by_id('id_password').send_keys("decide1234")
         self.driver.find_element_by_css_selector("input[type='submit']").click()
-        self.driver.get("http://127.0.0.1:8000/voting/question")
+        self.driver.get("http://decide-zapdos-votacion.herokuapp.com/voting/question")
         self.driver.find_element_by_css_selector("tr:nth-child(1) .btn-danger").click()
         self.driver.find_element_by_css_selector(".btn-danger").click()
 
@@ -25,12 +25,12 @@ class TestCreateQuestion():
         self.driver.quit
 
     def test_createQuestion(self):
-        self.driver.get("http://127.0.0.1:8000/admin/")
+        self.driver.get("http://decide-zapdos-votacion.herokuapp.com/admin/")
         self.driver.find_element_by_id('id_username').send_keys("decide")
         self.driver.find_element_by_id('id_password').send_keys("decide1234")
         self.driver.find_element_by_css_selector("input[type='submit']").click()
-        self.driver.get("http://127.0.0.1:8000/voting/question")
-        self.driver.get("http://127.0.0.1:8000/admin/voting/question/add/")
+        self.driver.get("http://decide-zapdos-votacion.herokuapp.com/voting/question")
+        self.driver.get("http://decide-zapdos-votacion.herokuapp.com/admin/voting/question/add/")
         self.driver.find_element_by_id('id_desc').send_keys("Test selenium")
         self.driver.find_element_by_id('id_options-0-number').send_keys("1")
         self.driver.find_element_by_id('id_options-0-option').send_keys("SI")
@@ -44,11 +44,11 @@ class TestSignup(unittest.TestCase):
         self.driver = webdriver.Firefox()
 
     def test_signup_fire(self):
-        self.driver.get("http://127.0.0.1:8000/admin/")
+        self.driver.get("http://decide-zapdos-votacion.herokuapp.com/admin/")
         self.driver.find_element_by_id('id_username').send_keys("decide")
         self.driver.find_element_by_id('id_password').send_keys("decide1234")
         self.driver.find_element_by_css_selector("input[type='submit']").click()
-        self.driver.get("http://127.0.0.1:8000/voting/question")
+        self.driver.get("http://decide-zapdos-votacion.herokuapp.com/voting/question")
         self.assertTrue(self.driver.find_element_by_class_name('card-header'))
 
     def tearDown(self):
